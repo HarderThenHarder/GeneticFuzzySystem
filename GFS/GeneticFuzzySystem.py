5fcb78be2db443dc66428cf5c79503e17ce8d1d8
Pky15882128547 <1414463123@qq.com>
2020-12-16 09:16:45 +0800

    add GFS

---
+++ GFS/GeneticFuzzySystem.py
@@ -0,0 +1,29 @@
+"""
+@Author: P_k_y
+@Time: 2020/12/14
+"""
+
+
+class GeneticFuzzySystem(object):
+
+    def __init__(self):
+        pass
+
+    def init_population(self):
+        """
+        初始化种群，一条染色体代表一个规则库，种群大小即为规则库的个数。
+        """
+        pass
+
+    def cross(self):
+        """
+        染色体交叉函数，用于交换两条染色体的一部分基因。
+        """
+        pass
+
+    def mutate(self):
+        """
+        基因变异函数，用于实现基因突变。
+        """
+        pass
+
