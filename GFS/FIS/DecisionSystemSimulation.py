20b14ee82ece353ff2842633522c3c54e63998d0
ARC <1414463123@qq.com>
2021-01-06 19:24:29 +0800

    Update DSSClass, WIP

--- GFS/FIS/DecisionSystemSimulation.py
+++ GFS/FIS/DecisionSystemSimulation.py
@@ -11,127 +11,106 @@ class DecisionSystemSimulation(object):
         """
         self.decision_system = decision_system
 
-    def simulation_get_action(self, input):
+    def simulation_get_action(self, input_info):
         """
-        根据输入与规则库进行模糊推理，计算出相应的决策输出
-        :param input: 输入模糊变量的具体数值
-        :return: 输出动作的枚举ID
+        根据输入与规则库进行模糊推理，遍历整个规则库，找到最符合当前输入条件的规则，求出输出模糊变量具有最高置信度的模糊程度(Term)，将其索引作为决策行为索引。
+        :param input_info: 输入模糊变量的观测数值
+        :return: 决策行为索引
         """
         rule_lib = self.decision_system.rule_lib  # 先获得FIS的规则库
-        fuzzy_variable_len = len(self.decision_system.fuzzy_variable_list)
-        input_fuzzy_variable = self.decision_system.fuzzy_variable_list[0:fuzzy_variable_len - 1]  # 输入模糊变量
-        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[fuzzy_variable_len - 1]  # 输出模糊变量
-        action_num = len(output_fuzzy_variable.terms)
-        action_value = [0 for index in range(0, action_num)]
+        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1]  # 输出模糊变量
+        action_value = [0 for _ in range(len(output_fuzzy_variable.terms))]   # 一个模糊程度（Term）对应一个action
+
+        """ 遍历整个规则库，根据输入来判断当前状态（观测）最符合哪一条规则前件，继而计算输出模糊变量的哪一个 Term 置信度最高，置信度最高的 Term 则为最置信度最高的行为 """
         for rule in rule_lib:
             antecedent = rule.antecedent
             consequent = rule.consequent
-            strength = antecedent.compute_value(input)  # 计算规则强度
-            action_id = consequent.clause.id
+            strength = antecedent.compute_value(input_info)  # 计算规则强度
+            action_id = consequent.clause.id            # Term 的 id 即行为的 id
             if action_id == -1:
                 continue
+            """ 多个规则可能对应同一个 action(Term)，取置信度（规则强度）最大的 action（Term）Value 作为该行为的 action value """
             action_value[action_id] = max(action_value[action_id], strength)
-        max_value = -1
-        opt_action = -1
-        for index in range(0, action_num):
-            if action_value[index] > max_value:
-                max_value = action_value[index]
-                opt_action = index
-        return opt_action
 
-    def simulation_get_crisp_value(self, input):
+        return action_value.index(max(action_value))    # 返回最高置信度行为的行为索引
+
+    def simulation_get_crisp_value(self, input_info):
         """
-        根据输入与规则库计算精确数值输出
-        @param input: 输入模糊变量的具体数值
-        @return: consequent经过去模糊化后的精确输出
+        根据输入与规则库进行模糊推理，遍历整个规则库，找到最符合当前输入条件的规则，求出输出模糊变量具有最高置信度的模糊程度(Term)，并将该 Term 去模糊化成具体数值输出。
+        @param input_info: 输入模糊变量的观测数值
+        @return: consequent 经过去模糊化后的精确输出
         """
         rule_base = self.decision_system.rule_lib  # 先获得FIS的规则库
-        fuzzy_variable_len = len(self.decision_system.fuzzy_variable_list)
-        input_fuzzy_variable_list = self.decision_system.fuzzy_variable_list[0:fuzzy_variable_len - 1]  # 输入模糊变量
-        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[fuzzy_variable_len - 1]  # 输出模糊变量
+        output_fuzzy_variable = self.decision_system.fuzzy_variable_list[-1]  # 输出模糊变量
         action_num = len(output_fuzzy_variable.terms)
-        term_temp = Term("temp", "temp", [0, 0, 0], -1)
-        output_terms_list = [Term for index in range(0, action_num)]
+        output_terms_list = [Term for _ in range(action_num)]                 # 一个模糊程度（Term）对应一个action
+
+        """ 将输出模糊变量的所有 Term 保存起来，一个 Term 对应一个 action """
         for k, v in output_fuzzy_variable.terms.items():
             output_terms_list[v.id] = v
 
-        clip_value = [0 for index in range(0, action_num)]
+        clip_value = [0 for _ in range(action_num)]
 
+        """ 遍历整个规则库，保存输出模糊变量所有模糊程度（Term）的置信度（规则强度） """
         for rule in rule_base:
             antecedent = rule.antecedent
             consequent = rule.consequent
-            strength = antecedent.compute_value(input)  # 计算规则强度
+            strength = antecedent.compute_value(input_info)  # 计算规则强度
             action_id = consequent.clause.id
             if action_id == -1:
                 continue
             clip_value[action_id] = max(clip_value[action_id], strength)
+
+        """ 使用每一个 Term 的 clip_value（规则强度）进行插值，求出插值之后的 universe """
         mf_value = []
-        # 使用每一个Term的clip_value进行插值，求出插值之后的universe
         interp_universe = []
         normal_universe = []
-        # print(clip_value)
+
+        """ 根据之前计算出的每一个 Term 在 x 轴取值范围的并集"""
         for term in output_fuzzy_variable.all_term():
             id = term.id
             clip = clip_value[id]
             a = term.trimf[0]
             b = term.trimf[1]
             c = term.trimf[2]
-            if math.fabs(clip - 1) < 0.000001:
+            if math.fabs(clip - 1) < 1e-6:
                 continue
             if a != b:
                 interp_universe.append(clip * (b - a) + a)
             if b != c:
                 interp_universe.append(c - clip * (c - b))
 
+        """ 输出模糊变量原本各 Term 的 x 轴取值范围 """
         for index in range(output_fuzzy_variable.universe_down, output_fuzzy_variable.universe_up + 1):
             normal_universe.append(index)
-        final_universe = np.union1d(interp_universe, normal_universe)
+        final_universe = np.union1d(interp_universe, normal_universe)   # np.union1d 求并集
 
-        for index in range(0, action_num):
+        for index in range(action_num):
             mf_value.append([])
-        for index_universe in range(0, len(final_universe)):
-            for index_action in range(0, action_num):
+
+        """ 根据每一个 Term（Action）的隶属函数值  """
+        for index_universe in range(len(final_universe)):
+            for index_action in range(action_num):
                 mf_value[index_action].append(
                     output_terms_list[index_action].compute_membership_value(final_universe[index_universe]))
 
         output_distribution = []
-        max_clip = max(clip_value)
-        sum1 = 0
-        count1 = 0
-        # for index in range(0, action_num):
-        #     if math.fabs(max_clip - clip_value[index]) < 0.000001:
-        #         for i in range(0, len(mf_value[index])):
-        #             if i - 1 >= 0:
-        #                 d1 = mf_value[index][i] - max_clip
-        #                 d2 = mf_value[index][i - 1] - max_clip
-        #                 if d1 >= 0.000001 and d2 <= 0.000001:
-        #                     inter_value = i - (mf_value[index][i] - max_clip) / (mf_value[index][i] - mf_value[index][i - 1])
-        #                     sum1 += inter_value
-        #                     count1 += 1
-        #                 elif d1 <= 0.00001 and d2 >= 0.000001:
-        #                     inter_value = (i - 1) + (mf_value[index][i - 1] - max_clip) / (mf_value[index][i - 1] - mf_value[index][i])
-        #                     sum1 += inter_value
-        #                     count1 += 1
 
-        for index_universe in range(0, len(final_universe)):
+        for index_universe in range(len(final_universe)):
             Max = 0
-            for index_action in range(0, action_num):
+            for index_action in range(action_num):
                 mf_value[index_action][index_universe] = min(mf_value[index_action][index_universe],
                                                              clip_value[index_action])
                 Max = max(Max, mf_value[index_action][index_universe])
             output_distribution.append(Max)
 
-        # 使用mean of maximum去模糊化分布
-        Max = 0
-        for value in output_distribution:
-            Max = max(Max, value)
+        """ 使用 mean of maximum 去模糊化分布 """
+        Max = max(output_distribution)
         count = 0
-        sum = 0
-        for i in range(0, len(output_distribution)):
-            if math.fabs(Max - output_distribution[i]) <= 0.000001:
-                sum += final_universe[i]
+        sum_count = 0
+        for i in range(len(output_distribution)):
+            if math.fabs(Max - output_distribution[i]) <= 1e-6:
+                sum_count += final_universe[i]
                 count += 1
-        sum += sum1
-        count += count1
 
-        return sum / count
+        return sum_count / count
